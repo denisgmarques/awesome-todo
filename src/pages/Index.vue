d5abfd519b7953d7ab4b66ca6d2c1af66bce3a45
Denis Giovan Marques <denis.marques@icolabora.com.br>
2019-11-21 15:37:17 -0300

    initial commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-page padding>
+    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
+    {{ message }}
+  </q-page>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: 'Olá mundão lindo!',
+    };
+  },
+  name: 'PageIndex',
+};
+</script>
