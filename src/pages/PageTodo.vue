8d5079d8b119ed0dae786d1ff9e26e03405e0673
Denis Giovan Marques <denis.marques@icolabora.com.br>
2020-04-04 18:16:53 -0300

    Page TODO.

--- src/pages/PageTodo.vue
+++ src/pages/PageTodo.vue
@@ -1,12 +1,82 @@
 <template>
-  <q-page padding>
-      <p>Todo page</p>
+  <q-page class="q-pa-md">
+    <q-list bordered>
+      <q-item-label header>Tasklist</q-item-label>
+
+      <q-item
+        v-for="task in tasks"
+        :key="task.id"
+        @click="task.completed = !task.completed"
+        clickable
+        v-ripple
+      >
+        <q-item-section side top>
+          <q-checkbox v-model="task.completed" />
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ task.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side top>
+          <div class="row">
+            <div class="column justify-center">
+              <q-icon
+                name="event_note"
+                size="18px"
+                class="q-mr-xs"
+              />
+            </div>
+            <div class="column">
+              <q-item-label
+                caption
+                class="row justify-end"
+              >
+                <small>{{ task.dueDate }}</small>
+              </q-item-label>
+              <q-item-label
+                caption
+                class="row justify-end"
+              >
+                <small>{{ task.dueTime }}</small>
+              </q-item-label>
+            </div>
+          </div>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      tasks: [
+        {
+          id: 1,
+          name: 'Go to supermarket',
+          completed: false,
+          dueDate: '05/04/2020',
+          dueTime: '18:30'
+        },
+        {
+          id: 2,
+          name: 'Get bananas',
+          completed: false,
+          dueDate: '05/04/2020',
+          dueTime: '18:31'
+        },
+        {
+          id: 3,
+          name: 'Get apples',
+          completed: false,
+          dueDate: '05/04/2020',
+          dueTime: '18:32'
+        }
+      ]
+    }
+  }
 }
 </script>
 
